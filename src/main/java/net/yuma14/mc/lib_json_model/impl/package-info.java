debeca4bd3d9c0aa480f817fe11c0c7002cb6669
yuma14 <yuma140902@gmail.com>
2023-08-02 21:45:39 +0900

    add package-info

---
+++ src/main/java/net/yuma14/mc/lib_json_model/impl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The classes in this package are classes that implement LibJsonModel functionality.
+ * Compatibility between versions is not guaranteed, so LibJsonModel users should not use them.
+ * Use the classes in the `api` package instead.
+ */
+package net.yuma14.mc.lib_json_model.impl;
